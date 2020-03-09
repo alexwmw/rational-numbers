f7e0ca58b87c2230c2ce083ad6c63e9630705438
Alex Wright <alexwmw@gmail.com>
2020-03-09 23:52:03 +0000

    Tinkering and trying things to no avail

--- RationalNumbers/RationalNumbers/InternalExtensions.cs
+++ RationalNumbers/RationalNumbers/InternalExtensions.cs
@@ -2,26 +2,25 @@
 using System.Runtime.CompilerServices;
 using RationalNumbers;
 
+
 [assembly: InternalsVisibleTo("RationalNumbers.Test")]
 namespace InternalExtensions
 {
+    
     //todo: test all of these!
     internal static class RationalNumberExtensions
     {
         private static int GcdOf( int numerator, int denominator) =>
             denominator == 0 ? numerator : GcdOf(denominator, numerator % denominator);
 
-        internal static IRationalNumber Reduce(this IRationalNumber number) =>
-            new RationalNumber(
-                number.Numerator.DivideByGcd(number.Numerator, number.Denominator),
-                number.Denominator.DivideByGcd(number.Numerator, number.Denominator));
+        internal static IRationalNumber Reduce(this IRationalNumber number) => throw new NotImplementedException();
+            //new ReducedRationalNumber(number.Numerator, number.Denominator);
 
+        internal static int DivideByGcd(this int dividend, int numerator, int denominator) =>
+            dividend / GcdOf(numerator, denominator);
 
-        internal static int DivideByGcd(this int i, int numerator, int denominator) =>
-            i / GcdOf(numerator, denominator);
-
-        internal static int ToProperNegativeForm(this int i, int denominator) =>
-            denominator < 0 ? i * -1 : i;
+        internal static int ToProperNegativeForm(this int original, int denominator) =>
+            denominator < 0 ? -original : original;
         
     }
 }
--- RationalNumbers/RationalNumbers/RationalNumber.cs
+++ RationalNumbers/RationalNumbers/RationalNumber.cs
@@ -5,7 +5,7 @@ using static InternalExtensions.RationalNumberExtensions;
 
 namespace RationalNumbers
 {
-    public struct RationalNumber : IRationalNumber
+    public struct RationalNumber : IRationalNumber // IComparable? IConvertible?
     {
         public int Numerator { get; private set; }
         public int Denominator { get; private set; }
@@ -18,15 +18,14 @@ namespace RationalNumbers
         public RationalNumber(int numerator, int denominator)
         {
             if (denominator == 0)
-                throw new ArgumentException("Denominator cannot be zero."); //todo: test this
+                throw new DivideByZeroException(); //todo: test this
 
-            Numerator = numerator
-                .DivideByGcd(numerator, denominator)
-                .ToProperNegativeForm(denominator);
+            Numerator = numerator;
+            //.ToProperNegativeForm(denominator);
 
-            Denominator = denominator
-                .DivideByGcd(numerator, denominator)
-                .ToProperNegativeForm(denominator);
+            Denominator = denominator;
+            //.ToProperNegativeForm(denominator);
+
         }
 
         public static IRationalNumber operator +(RationalNumber r1, RationalNumber r2) => r1.Add(r2);
@@ -57,26 +56,38 @@ namespace RationalNumbers
          * a1/b1 + a2/b2 =
          * (a1 * b2 + a2 * b1) / (b1 * b2).
          */
-        public IRationalNumber Add(IRationalNumber number) => new RationalNumber(
-            this.Numerator * number.Denominator + this.Denominator * number.Numerator,
-            this.Denominator * number.Denominator);
+        public IRationalNumber Add(IRationalNumber number) {
+            int num = this.Numerator * number.Denominator + this.Denominator * number.Numerator;
+            int den = this.Denominator * number.Denominator;
+
+            return new RationalNumber(num, den);
+        }
 
 
         /**
          * The difference of two rational numbers r1 = a1/b1 and r2 = a2/b2 is r1 - r2 =
          * a1/b1 - a2/b2 = (a1 * b2 - a2 * b1) / (b1 * b2).
          */
-        public IRationalNumber Subtract(IRationalNumber number) => new RationalNumber(
-            this.Numerator * number.Denominator - this.Denominator * number.Numerator,
-            this.Denominator * number.Denominator);
+        public IRationalNumber Subtract(IRationalNumber number)
+        {
+            int num = this.Numerator * number.Denominator - this.Denominator * number.Numerator;
+            int den = this.Denominator * number.Denominator;
+
+            return new RationalNumber(num, den);
+        }
 
         /**
          * The product (multiplication) of two rational numbers r1 = a1/b1 and r2 =
          * a2/b2 is r1 * r2 = (a1 * a2) / (b1 * b2).
          */
-        public IRationalNumber Multiply(IRationalNumber number) => new RationalNumber(
-            this.Numerator * number.Numerator,
-            this.Denominator * number.Denominator);
+        public IRationalNumber Multiply(IRationalNumber number)
+        {
+            int num = this.Numerator * number.Numerator;
+            int den = this.Denominator * number.Denominator;
+
+            return new RationalNumber(num, den);
+        }
+
         
         /**
          */
@@ -95,12 +106,15 @@ namespace RationalNumbers
             var number = obj as IRationalNumber;
             if (number == null)
                 return false;
-            return this.Numerator * number.Denominator == this.Denominator * number.Numerator;
+
+            return this.Numerator * number.Denominator
+                == this.Denominator * number.Numerator;
         }
 
         public override int GetHashCode()
         {
-            return (Numerator / Denominator + Denominator / Numerator) * 100;
+            int prime = 31;
+            return (Numerator / Denominator + Denominator / Numerator) * prime;
         }
 
         // plus any other methods you deem necessary to meet the specification
